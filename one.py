6c2011fac9dfbf06f16365b3952774a225a07730
Nikhil Patil <nikhilpatilvit@gmail.com>
2026-05-19 12:03:57 +0530

    Create one.py

---
+++ one.py
@@ -0,0 +1,106 @@
+#nikhil patil
+
+# 1. Create a list of 10 numbers and print the list.
+# list=[1,2,3,4,5,6,7,8,9,10]
+# print(list)
+
+# 2. Create a list containing different data types (integer, float, string, boolean) and print its type.
+# list1=[1,1.5,"tom",True]
+# print(type(list1))
+
+# 3. Access and print the first, middle, and last elements from a list of your choice.
+# list3=[10,20,30]
+# print(list3[0])
+# res=len(list3)
+# res1=res//2
+# print(list3[res1])
+# # print(list3[-1])
+
+# 4. Slice the list to print the first 3 elements and last 3 elements separately.
+# l4=[1,2,3,4,5,6]
+# print(l4[0:3:1])
+# print(l4[-1:-4:-1])
+
+# 5. Replace the second element of a list with a new value.
+# l5=[1,2,3,4,5]
+# l5[1]=40
+# print(l5)
+
+# 6. Add a new element at the end of a list using append() and display the updated list.
+# l6=[10,20,30,40]
+# l6.append("tom")
+# print(l6)
+
+# 7. Insert an element at index 2 using insert() and print the updated list.
+# l7=[1,2,3,4,5,6]
+# l7.insert(2,"value")
+# print(l7)
+
+# 8. Extend the list with another list of your choice and display the final list.
+# l8=[1,2,3]
+# l=[40,50,60]
+# l8.extend(l)
+# print(l8)
+
+# 9. Remove a specific element by value using remove() and display the result.
+# l9=[1,2,'tom']
+# l9.remove("tom")
+# print(l9)
+
+# 10. Remove the element at index 3 using pop() and display which element was removed.
+# l10=[1,2,3,4,5]
+# ele=l10.pop(3)
+# print("removed element:",ele)
+
+# 11. Delete the element at index 0 using del keyword and print the remaining list.
+# l11=[1,2,3,4,5,6]
+# del l11[0:1]
+# print("after del:",l11)
+
+# 12. Clear all elements from the list using clear() and print the empty list.
+# l12=[1,2,3,4,5,6]
+# l12.clear()
+# print(l12)
+
+# 13. Recreate a list and check whether a specific element (for example "apple") exists in the list using the 'in' operator.
+# l13=[1,"apple","banana",2.5]
+# print("apple" in l13)
+
+
+# 14. Find the index position of a specific element in the list using the index() method.
+# l14=[10,20,30,40]
+# print(l14.index(20))
+
+# 15. Count how many times a specific value appears in the list using count().
+# l15=[1,2,3,4,5,2,6,7,2]
+# print(l15.count(2))
+
+# 16. Sort a list of numbers in ascending order using sort() and display the result.
+# l16=[3,1,6,5,2,4]
+# l16.sort()
+# print(l16)
+
+# 17. Sort a list of strings in descending order using sort(reverse=True) and display the result.
+# l17=[1,2,3,4,5,6]
+# l17.sort(reverse=True)
+# print(l17)
+
+# 18. Reverse the order of a list using reverse() and print the updated list.
+# l18=[1,2,3,4,5,6]
+# l18.reverse()
+# print(l18)
+
+# 19. Copy the contents of one list to another using the copy() method and modify one list to see if it affects the other.
+# l19=[1,"tom",2,3,4,5]
+# copied=l19.copy()
+# print("result:",copied)
+
+# 20. Combine two lists using the + operator and print the combined list.
+# l20=[1,2,3,4]
+# l21=[5,6,7,8]
+# result=l20+l21
+# print(result)
+
+
+
+
